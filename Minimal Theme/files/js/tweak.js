4d4db842535ab514794eb3806efa84cde81dfc3f
nogira <83847986+nogira@users.noreply.github.com>
2021-07-03 00:55:57 +1000

    fixed editor.css refresh field bug
    fixed bug where in browse screen, if you started with a card open with lets say 2 fields the editor.css would apply to both fields, but when you switched to a card with >2 fields, the first two fields would still have the css, but the fields after that would not have the css, because the editor.css DOM addition only occured when the window loaded, not when the window updated.
    
    now the editor.css is reapplied when shadowroot is updated

--- Minimal Theme/files/js/tweak.js
+++ Minimal Theme/files/js/tweak.js
@@ -4,17 +4,52 @@ var fullUrl = script.src;
 newpath = fullUrl.replace("js/tweak.js", "css/editor.css");
 //alert(newpath);
 
+
+// add editor.css to shadowroot that contains field
 function main_func() {
-    var field_nodes = document.querySelector("#fields").childElementCount
+    var field_nodes = document.querySelector("#fields").childElementCount;
 
     for (i = 1; i != (field_nodes + 1); i++) {
-
         var new3 = document.createElement("link");
         new3.setAttribute("rel", "stylesheet");
         new3.setAttribute("href", newpath);
         var node = document.querySelector(`#fields > div:nth-child(${i}) > div.field`).shadowRoot;
-        node.insertBefore(new3, node.childNodes[1]);
-    } 
-}
-//                                                       function    ms
-document.body.addEventListener("load", window.setTimeout(main_func, 100));
+
+        // to prevent infite additions of editor.css when field is updated in 
+        // browse, only add if not present (i.e. child nodes < 4)
+        if (node.childElementCount < 4) {
+            node.insertBefore(new3, node.childNodes[1]);
+            alert('added css');
+        };
+        
+    };
+};
+//                function    ms
+// window.setTimeout(main_func, 100)
+
+
+
+// Select the node that will be observed for mutations
+const targetNode = document.getElementById('fields');
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = function(mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    for(const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            main_func();
+        }
+    }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
+// Later, you can stop observing
+// observer.disconnect();
